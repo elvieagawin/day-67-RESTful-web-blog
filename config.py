c497b3a3e1873acdebe03908ad24fa11d3842a98
elvie <pauagawin21@gmail.com>
2021-08-12 16:06:50 +0800

    Modified requirements.txt and change SQLite from PostgreSQL.

--- config.py
+++ config.py
@@ -2,9 +2,10 @@ from Google import Create_Service
 import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import os
 
 
-CLIENT_SECRET_FILE = 'n5uQPoAZHjneGdQ_SVwLFa9W'
+CLIENT_SECRET_FILE = os.environ.get("CLIENT_SECRET_KEY")
 API_NAME = 'gmail'
 API_VERSION = 'v1'
 SCOPES = ['https://mail.google.com/']
